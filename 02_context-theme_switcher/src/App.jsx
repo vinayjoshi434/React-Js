3a9c332137068805f02b48cafe11aba5ee46718c
vinayjoshi434 <vinayjoshi434@gmail.com>
2024-09-27 15:53:13 +0530

    theme switcher with tailwind functionality of toggle the theme based on class .. with help of context Api 2nd way of declaration…

--- 02_context-theme_switcher/src/App.jsx
+++ 02_context-theme_switcher/src/App.jsx
@@ -6,11 +6,13 @@ import Themebtn from "./components/Themebtn";
 import Card from "./components/Card";
 
 function App() {
-const[themeMode ,setthemeMode]=useState("light")
+const[themeMode ,setthemeMode]=useState("light")// statevariable
 
 const toggleTheme=()=>{
     setthemeMode((prev)=>(prev==="light"? "dark":"light"))
 }
+
+//basically this runs when thememode changes and this will change the class accordingly
 useEffect(()=>{
     document.querySelector("html").classList.remove("dark","light")
     document.querySelector("html").classList.add(themeMode)
@@ -19,7 +21,8 @@ useEffect(()=>{
 },[themeMode])
 
   return (
-    <ThemecontextProvider value={{themeMode, toggleTheme}}>
+    // here we need to wrap this parent component with the ThemeContextprovider in order to provide global access of value in ThemeContextProvider
+    <ThemecontextProvider value={{themeMode, toggleTheme}}>{/* themeMode and toggleTheme are accessible globally in entire wrapped components with the help of use state        */}
       <div className="flex flex-wrap min-h-screen items-center ">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
--- 02_context-theme_switcher/src/components/Themebtn.jsx
+++ 02_context-theme_switcher/src/components/Themebtn.jsx
@@ -5,6 +5,8 @@ function Themebtn() {
 
 const{themeMode,toggleTheme}=useTheme();
 
+// accessing the theme mode and toggleSwitch 
+
 const handelbtnchange=(e)=>{
     toggleTheme()
 }     
@@ -15,7 +17,7 @@ const handelbtnchange=(e)=>{
                 type="checkbox"
                 onChange={handelbtnchange}
                 className="sr-only peer"
-                checked={themeMode==="dark" }
+                checked={themeMode==="dark" }// this will checked accordingly to the condition checked if results out true 
                 
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
